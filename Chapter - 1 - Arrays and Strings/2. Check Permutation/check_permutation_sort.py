af997e00f5c52a0ab3c1d5bdaad5dfbb03f967f8
jaintavish <jaintavish@gmail.com>
2019-06-01 13:22:35 +0530

    Check Permutation

--- Chapter - 1 - Arrays and Strings/2. Check Permutation/check_permutation_sort.py
+++ Chapter - 1 - Arrays and Strings/2. Check Permutation/check_permutation_sort.py
@@ -3,6 +3,7 @@ def listify(str):
 	for i in str:
 		lis.append(i)
 	return lis
+
 def check_permutation(str1,str2):
 	if len(str1)!=len(str2):
 		return False
@@ -12,7 +13,7 @@ def check_permutation(str1,str2):
 	else:
 		return False
 
-str1=input("Enter First String: ")
-str2=input("Enter Second String: ")
+str1 = input("Enter First String: ")
+str2 = input("Enter Second String: ")
 print(check_permutation(str1,str2))
 
