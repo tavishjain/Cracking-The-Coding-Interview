3e8c0a1465b2afdad374360baf9e2c8d71b21881
jaintavish <jaintavish@gmail.com>
2019-06-01 12:49:37 +0530

    Is Unique

---
+++ Chapter - 1 - Arrays and Strings/1. Is Unique/Is Uniqe.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void isUnique(String str){
+        if(str.length() > 128){
+            System.out.println("There are repeating characters in this String");
+            return;
+        }else{
+            boolean[] present = new boolean[128];
+            for(char ch : str.toCharArray()){
+                if(present[ch]){
+                    System.out.println("There are repeating characters in this String");
+                    return;
+                }
+                present[ch] = true;
+            }
+            System.out.println("There are no repeating characters in this String");
+        }
+    }
+
+    public static void main(String[] args) { // unit testing (optional)
+
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        isUnique(str);
+
+    }
+}
