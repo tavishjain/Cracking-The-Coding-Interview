bfe0a6c75a9ec1218f106b9bb254e20e2539e464
tavishjain <jaintavish@gmail.com>
2020-10-31 01:27:41 +0530

    Added question 5.1

---
+++ Chapter - 5 - Bit Manipulation/5.1 Insertion.cpp
@@ -0,0 +1,19 @@
+#include <cstdint>
+#include <iostream>
+#include "bitutils.hpp"
+
+int32_t insertion(int32_t N, int32_t M, int i, int j)
+{
+    int w = j - i;
+    if (w <= 0)
+        return N;
+
+    int32_t maskN = (~0 << (j + 1)) | ((1 << i) - 1);
+    return (N & maskN) | (M << i);
+}
+
+int main()
+{
+    auto result = insertion(0b10000000000, 0b10011, 2, 6);
+    std::cout << bits<decltype(result), 11>(result);
+}
